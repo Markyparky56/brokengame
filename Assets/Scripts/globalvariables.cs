62062d8850cddd8251548b565926e3798950be29
U-Lenovo\Mark <Mark@Lenovo.(none)>
2015-01-25 11:33:49 +0000

    Beginning to add triggers

---
+++ Assets/Scripts/globalvariables.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class globalvariables : MonoBehaviour {
+
+    public enum level { Title, Start, Broken, End}
+    public level sceneLevel;
+    //public level
+
+	// Use this for initialization
+	void Start () 
+    {
+        sceneLevel = level.Broken; // Change to title when finished testing
+	}	
+}
---
+++ Assets/Scripts/spikeTrigger.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spikeTrigger : MonoBehaviour {
+
+    public GameObject invisibleWall;
+    globalvariables global;
+
+	// Use this for initialization
+	void Start () {
+        global = GameObject.Find("gameManager").GetComponent<globalvariables>();
+  	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if (global.sceneLevel == globalvariables.level.Broken)
+        {
+            if (collision.gameObject.name == "bobController")
+            {
+                Destroy(invisibleWall);
+            }
+        }
+    }
+}
